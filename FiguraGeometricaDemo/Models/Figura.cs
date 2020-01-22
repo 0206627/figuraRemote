36701c212cd673c9bfdd6d4cf296f5e754b78172
Mariana De la Vega <0206627@up.edu.mx>
2020-01-21 19:51:12 -0600

    agregar atributo 4

--- FiguraGeometricaDemo/Models/Figura.cs
+++ FiguraGeometricaDemo/Models/Figura.cs
@@ -6,12 +6,14 @@ namespace FiguraGeometricaDemo.Models
         public int atributo1 { set; get; }
         private int _atributo2 { set; get; }
         protected int atributo3 { set; get; }
+        public int atributo4 { set; get; }
 
         public Figura()
         {
             atributo1 = 1;
             _atributo2 = 1;
             atributo3 = 1;
+            atributo4 = 1;
         }
 
     }
